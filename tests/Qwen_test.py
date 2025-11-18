6197a2775863f11100b3096ab06078571b9fe28a
Sota Mark Ogata <ogata@berkeley.edu>
2025-11-18 00:53:51 +0000

    local image captioning

--- tests/Qwen_test.py
+++ tests/Qwen_test.py
@@ -28,7 +28,7 @@ messages = [
         "content": [
             {
                 "type": "image",
-                "image": "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg",
+                "image": "outputs/captioners/frame_30.jpg",
             },
             {"type": "text", "text": "Describe this image."},
         ],
@@ -50,7 +50,7 @@ inputs = processor(
 inputs = inputs.to("cuda")
 
 # Inference: Generation of the output
-generated_ids = model.generate(**inputs, max_new_tokens=128)
+generated_ids = model.generate(**inputs, max_new_tokens=512)
 generated_ids_trimmed = [
     out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
 ]
